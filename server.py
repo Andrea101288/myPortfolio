388f4cd0dd3ef2cbf1b49bcee0f5cf56be13835d
Andrea Mancini <andrea101288@hotmail.it>
2020-05-15 22:58:54 +0200

    first portfolio commit

---
+++ server.py
@@ -0,0 +1,20 @@
+# Import FlaskLibrary
+from flask import Flask, render_template, url_for
+
+# We use the flask class to instanciate a class
+app = Flask(__name__)
+print(__name__)
+
+# this is a Decorator! We already saw it
+@app.route('/')
+def my_home():    
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):    
+    return render_template(page_name)
+
+
+
+
+
